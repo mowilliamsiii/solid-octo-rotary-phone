15dedb01c82050016e920f6495940a68860274a9
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-07-20 09:21:09 -0400

    First add. Limits controller

---
+++ src/main/java/com/moseswilliamsiii/RatezServiceApplication.java
@@ -0,0 +1,13 @@
+package com.moseswilliamsiii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatezServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatezServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moseswilliamsiii/RatezServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moseswilliamsiii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatezServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
