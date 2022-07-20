eb75a2e5e2b16e3a9a9e57f17a3bf7625960ed44
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-07-20 11:04:50 -0400

    limits hard coded Configuration

--- src/main/java/com/moseswilliamsiii/controllers/LimitsController.java
+++ src/main/java/com/moseswilliamsiii/controllers/LimitsController.java
@@ -1,14 +1,19 @@
 package com.moseswilliamsiii.controllers;
 
+import com.moseswilliamsiii.configuration.Configuration;
 import com.moseswilliamsiii.model.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits(){
-        return new Limits(1,1000);
+        return new Limits(configuration.getMin(), configuration.getMax());
     }
 }
